9211c64d6316ff164b2f9ea6aaabe1238b82edd9
shreyasetty94 <shreya.setty94@gmail.com>
2025-08-29 14:28:25 +0530

    Create Encapsulation.cs

---
+++ Encapsulation.cs
@@ -0,0 +1,37 @@
+using System;
+
+class SmartMeterAccount
+{
+    private int balance;
+
+    public void Recharge(int amount)
+    {
+        balance += amount;
+        Console.WriteLine($"Balance after recharge: {balance}");
+    }
+
+    public void Consume(int amount)
+    {
+        if (amount <= balance)
+        {
+            balance -= amount;
+            Console.WriteLine($"Balance after consumption: {balance}");
+        }
+        else
+        {
+            Console.WriteLine("Insufficient balance");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        SmartMeterAccount account = new SmartMeterAccount();
+
+        account.Recharge(500);
+        account.Consume(200);
+        account.Consume(400);
+    }
+}
